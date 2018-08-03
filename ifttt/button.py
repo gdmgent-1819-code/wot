2071c29ae5d7c29ccfe9daf0f3e1889dbf70426d
drdynscript <drdynscript@gmail.com>
2018-08-03 14:35:57 +0200

    Add button.py to ifttt folder

---
+++ ifttt/button.py
@@ -0,0 +1,9 @@
+# Event: nmd_wot_button_pressed
+# Documentation: https://ifttt.com/services/maker_webhooks/settings
+# Settings: https://ifttt.com/services/maker_webhooks/settings
+# Connection: https://maker.ifttt.com/use/c9KgUA_naJd3ZDRJbFb26t
+# Test URL: curl -X POST https://maker.ifttt.com/trigger/nmd_wot_button_pressed/with/key/dNKChU0OqeQvY2s-SJO3ed
+'''
+drdynscript@MacBook-Pro-van-Philippe:~/ahs/gdm/gdmgent-1819-code/wot/ifttt$ curl -X POST https://maker.ifttt.com/trigger/nmd_wot_button_pressed/with/key/dNKChU0OqeQvY2s-SJO3ed
+Congratulations! You've fired the nmd_wot_button_pressed eventdrdynscript@MacBook-Pro-van-Philippe:~/ahs/gdm/gdmgent-1819-code/wot/ifttt$
+'''
