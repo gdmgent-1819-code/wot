34ae66ba6c1f1f08a02c85f9a979472f90f4c210
basismit <basismit@student.artevelde.be>
2018-08-03 20:31:18 +0200

    Add Gmail IFTTT PubNub Example

--- ifttt/button.py
+++ ifttt/button.py
@@ -7,3 +7,33 @@
 drdynscript@MacBook-Pro-van-Philippe:~/ahs/gdm/gdmgent-1819-code/wot/ifttt$ curl -X POST https://maker.ifttt.com/trigger/nmd_wot_button_pressed/with/key/dNKChU0OqeQvY2s-SJO3ed
 Congratulations! You've fired the nmd_wot_button_pressed eventdrdynscript@MacBook-Pro-van-Philippe:~/ahs/gdm/gdmgent-1819-code/wot/ifttt$
 '''
+# Library: pip3 install requests
+from sense_hat import SenseHat, ACTION_PRESSED
+from time import sleep
+import requests
+import time
+import json
+import platform
+
+headers_json = {
+    'Content-type': 'application/json',
+    'Accept': 'text/plain'
+}
+
+def launchIFTTTWebRequest():
+    data = {
+        'value1': time.time(),
+        'value2': platform.uname()[1]
+    }
+    r = requests.post('https://maker.ifttt.com/trigger/nmd_wot_button_pressed/with/key/dNKChU0OqeQvY2s-SJO3ed', data=json.dumps(data), headers=headers_json)
+    print('Sent request to IFTTT Webhook')
+    
+sense = SenseHat()
+
+while True:
+    event = sense.stick.wait_for_event()
+    if event.action == 'pressed' and event.direction == 'middle':
+        print("The joystick was {} {}".format(event.action, event.direction))
+        launchIFTTTWebRequest()
+        
+    sleep(0.1)
---
+++ ifttt/gmail_and_pubnub_led.py
@@ -0,0 +1,50 @@
+# Library: sudo pip3 install 'pubnub>=4.1.0'
+# Event: nmd_wot_button_pressed
+# Documentation: https://ifttt.com/services/maker_webhooks/settings
+# Settings: https://ifttt.com/services/maker_webhooks/settings
+# Connection: https://maker.ifttt.com/use/c9KgUA_naJd3ZDRJbFb26t
+from sense_hat import SenseHat
+from time import sleep
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+import time
+import json
+import platform
+
+pnconfig = PNConfiguration()
+ 
+pnconfig.subscribe_key = 'pub-c-7a9982ea-c39a-4d66-b1ca-1c5138235e7'
+pnconfig.publish_key = 'sub-c-857c7620-9722-11e8-979a-3665db0876c0'
+ 
+pubnub = PubNub(pnconfig)
+
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+ 
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+ 
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            pubnub.publish().channel("awesomeChannel").message("hello!!").async(my_publish_callback)
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+ 
+    def message(self, pubnub, message):
+        pass  # Handle new message stored in message.message
+ 
+ 
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('ledChannel').execute()
