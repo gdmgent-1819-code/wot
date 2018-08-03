4451e5b5c7780cc474aa978d96000b39baa77cae
drdynscript <drdynscript@gmail.com>
2018-08-03 20:34:22 +0200

    Changed the keys for pubnub

--- ifttt/gmail_and_pubnub_led.py
+++ ifttt/gmail_and_pubnub_led.py
@@ -14,10 +14,10 @@ import json
 import platform
 
 pnconfig = PNConfiguration()
- 
-pnconfig.subscribe_key = 'pub-c-7a9982ea-c39a-4d66-b1ca-1c5138235ec7'
-pnconfig.publish_key = 'sub-c-857c7620-9722-11e8-979a-3665db0876c0'
- 
+
+pnconfig.publish_key = 'pub-c-7a9982ea-c39a-4d66-b1ca-1c5138235ec7' 
+pnconfig.subscribe_key = 'sub-c-857c7620-9722-11e8-979a-3665db0876c0'
+
 pubnub = PubNub(pnconfig)
 
 class MySubscribeCallback(SubscribeCallback):
